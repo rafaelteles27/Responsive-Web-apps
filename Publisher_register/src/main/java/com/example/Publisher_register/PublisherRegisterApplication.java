ee0190d885e8f060b24824164ac713e0afbea2cf
rafateles19 <rteles27@gmx.com>
2024-01-14 23:24:53 +0000

    starting the java backend by preparing the mongodb database credentials

---
+++ Publisher_register/src/main/java/com/example/Publisher_register/PublisherRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.example.Publisher_register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublisherRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublisherRegisterApplication.class, args);
+	}
+
+}
